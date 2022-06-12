7241ca5a96830536662b0d0c3606bdcddf7ec420
engineer-kadir <kadiryildizmeb@gmail.com>
2022-06-12 14:10:13 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import "tailwindcss/tailwind.css";
+import "../styles/globals.scss";
+import React, { useState, useEffect } from "react";
+import { Layout } from "../components";
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
+}
+
+export default MyApp;
